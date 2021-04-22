85e031061f62c72be36707b04450ee1a0226b273
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-04-22 13:05:03 +0300

    feat: added WhiteSpaceTokenizer

---
+++ Class8/WhiteSpaceTokenizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokenizer
+{
+    class WhiteSpaceTokenizer : Tokenizable
+    {
+        public override bool tokenizable(Tokenizer tokenizer)
+        {
+            return tokenizer.input.peek() == ' ';
+        }
+
+        public override Token tokenize(Tokenizer tokenizer)
+        {
+            Input input = tokenizer.input;
+            Token token = new Token(input.Position, input.LineNumber, "WhiteSpace", "");
+
+            while(input.peek() == ' ')
+                token.Value += input.step().Character;
+
+            if (token.Value.Length == 0)
+                return null;
+
+            return token;
+        }
+    }
+}
